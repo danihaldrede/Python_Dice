725021a6ecc1f031dc89789f86ab86128da977e4
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-05-04 17:45:26 -0400

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,13 @@
+# Dice Rolling Simulator 
+
+import random 
+
+def dice_roll():
+  diceRoll = random.randint(1,6)
+  return diceRoll
+print(dice_roll())
+print(dice_roll())
+print(dice_roll())
+print(dice_roll())
+print(dice_roll())
+print(dice_roll())
