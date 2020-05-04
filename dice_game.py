4e46665386139cfaa2e9455ccc973c663657046e
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-05-04 18:09:06 -0400

    Create dice_game.py

---
+++ dice_game.py
@@ -0,0 +1,42 @@
+# Dice Game Simulator
+
+import random
+
+def main():
+  player1 = 0
+  player1wins = 0
+  player2 = 0
+  lpayer2wins = 0 
+  rounds = 1
+  
+  while rounds!= 10:
+    print("Round" + str(rounds))
+    player1= dice_roll()
+    player2= dice_roll()
+    print("Player 1 Roll" + str(player1))
+    print("Player 2 Roll" + str(player2))
+    
+    if player1 == player2:
+      print("Draw!\n")
+    elif player1 > player2: 
+      player1wins = player1 wins + 1
+      print("Player 1 wins!\n")
+    else:
+      player2wins = player2wins + 1
+      print("Player 2 wins!\n")
+    rounds = rounds + 1
+  
+  if player1wins == player2wins: 
+      print("Draw")
+  elif player1wins > player2wins
+      print("Player 1 Wins - Rounds Won: str(player1wins))
+  else: 
+      print("Player 2 Wins - Rounds Won: str(player2wins))
+      
+def dice_roll():
+  diceRoll = random.randint(1, 6)
+  return diceRoll 
+
+main()
+  
+    
